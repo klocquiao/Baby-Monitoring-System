aa906da45cd3483963f3a9a876117abf7fa567cf
Kyle Locquiao <klocquiao45@gmail.com>
2022-03-13 15:52:00 -0700

    Create prototype structure for Nodejs and camera communication
    Each second a ping from the Nodejs server will be sent to the C++ program where the receiver class will respond to it with the frame from the camera.cpp class.

--- Webcam/camera.cpp
+++ Webcam/camera.cpp
@@ -5,46 +5,69 @@ https://github.com/derekmolloy/boneCV
 ***********************************************/
 
 
-
-#include <iostream>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/opencv.hpp>
-#include <time.h>
-#include <unistd.h>
-
+#include "camera.h"
 using namespace std;
 using namespace cv;
 
-int main() {
+static pthread_t cameraID;
+static pthread_mutex_t cameraMutex = PTHREAD_MUTEX_INITIALIZER;
+static void* cameraRunner(void* arg);
+static bool isStopping = false;
+static vector<uchar> buff;
 
+void startCamera() {
+    pthread_create(&cameraID, NULL, cameraRunner, NULL);
+}
+
+void stopReceiver(void) {
+    isStopping = true;
+    pthread_join(cameraID, NULL);
+}
+
+vector<uchar> getFrame() {
+    return buff;
+}
+
+void* cameraRunner(void* arg) {
   // setup the camera settings (640x480 image)
-  VideoCapture capture(0);
+    VideoCapture capture(0);
 
-  capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
-  capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+    capture.set(CAP_PROP_FRAME_WIDTH, 640);
+    capture.set(CAP_PROP_FRAME_HEIGHT, 480);
 
-  if (!capture.isOpened()) {
-    cout << "Failed to connect to the camera." << endl;
-  }
-
-  Mat frame;
-
-  while (1) {
-    // capture and process images from the webcam
-    capture >> frame;
-    
-    if (frame.empty()) {
-      cout << "Failed to capture an image" << endl;
-      return 0;
+    if (!capture.isOpened()) {
+        cout << "Failed to connect to the camera." << endl;
+        exit(0);
     }
 
-    std::vector<uchar> buff;
+    Mat frame;
 
+  while (!isStopping) {
+
+    // capture and process images from the webcam
+    pthread_mutex_lock(&cameraMutex);
+    {
+        capture >> frame;
+        
+        if (frame.empty()) {
+            cout << "Failed to capture an image" << endl;
+            return 0;
+        }
+
+        buff.clear();    
     // encode to jpg
     cv::imencode(".jpg", frame, buff);
+
+    }
+    pthread_mutex_unlock(&cameraMutex);
+
 
     // write jpg to stdout so it can be piped
     fwrite(buff.data(), buff.size(), 1, stdout);
     fflush(stdout);
   }
 }
+
+
+
+
