940b74915b6c35db2762112f5c0e5838e2a234f3
Kyle Locquiao <klocquiao45@gmail.com>
2022-03-13 16:00:12 -0700

    Commit/push unadded files

---
+++ Webcam/receiver.h
@@ -0,0 +1,20 @@
+#ifndef RECEIVER_H
+#define RECEIVER_H
+
+#include "camera.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <vector>
+#include <algorithm>
+
+void startReceiver();
+void stopReceiver(void);
+
+#endif
