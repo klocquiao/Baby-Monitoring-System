4395e344d0a7d3fe3d5d20671741e44afb404726
Kyle Locquiao <klocquiao45@gmail.com>
2022-03-27 19:18:51 -0700

    Implement UDP functionality for video recording (UNTESTED)
    Upon clicking the Record button on the website, the user will trigger the bbg to start recording the livestream footage. 30 seconds of footage will be recorded and the record button will be disabled for 45.
    
    NOTE: This has not be tested. Testing will happen during the next team meeting where everyone has access to the webcam bbg.

--- webserver/public/javascripts/udp_ui.js
+++ webserver/public/javascripts/udp_ui.js
@@ -9,10 +9,22 @@ $(document).ready(function() {
 		sendMonitorCommand("test");
 	});
 
+	$('#functionRecord').click(function(){
+		sendMonitorCommand("record");
+	});
+
 	socket.on('commandTest', function(result) {
 		$('#status-text').text(result);
 	});
-	
+
+	socket.on('commandRecord', function(result) {
+		$("#functionRecord").prop("value", "Recording...");
+		$("#functionRecord").attr('disabled','disabled');
+
+		setTimeout(function() {
+		$("#functionRecord").removeAttr('disabled');
+        }, 45000);
+	});
 });
 
 function sendMonitorCommand(message) {
