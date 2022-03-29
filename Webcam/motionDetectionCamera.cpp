3e6fdf1ff0dd9256091bb5bd4dc4ee7d2601f157
Taha Liaqat <tliaqat@sfu.ca>
2022-03-28 22:26:51 -0700

    webcam works along with motion detection. moved camera.cpp code into motionDetectionCamera.cpp

--- Webcam/motionDetectionCamera.cpp
+++ Webcam/motionDetectionCamera.cpp
@@ -29,33 +29,55 @@ int main() {
   cvtColor(frame, firstFrame, COLOR_BGR2GRAY);
   GaussianBlur(firstFrame, firstFrame, Size(21, 21), 0);
 
+  
+    int k = 0;
   while(camera.read(frame)) {
 
-      //convert to grayscale
-      cvtColor(frame, gray, COLOR_BGR2GRAY);
-      GaussianBlur(gray, gray, Size(21, 21), 0);
+    if(k == 100) {
 
-      //compute difference between first frame and current frame
-      absdiff(firstFrame, gray, frameDelta);
-      threshold(frameDelta, thresh, 25, 255, THRESH_BINARY);
+        cvtColor(frame, firstFrame, COLOR_BGR2GRAY);
+        GaussianBlur(firstFrame, firstFrame, Size(21, 21), 0);
+
+        k = 0;
+    }
+
+    //convert to grayscale
+    cvtColor(frame, gray, COLOR_BGR2GRAY);
+    GaussianBlur(gray, gray, Size(21, 21), 0);
+
+    //compute difference between first frame and current frame
+    absdiff(firstFrame, gray, frameDelta);
+    threshold(frameDelta, thresh, 25, 255, THRESH_BINARY);
       
-      dilate(thresh, thresh, Mat(), Point(-1,-1), 2);
-      findContours(thresh, cnts, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    dilate(thresh, thresh, Mat(), Point(-1,-1), 2);
+    findContours(thresh, cnts, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+      
 
       for(int i = 0; i< cnts.size(); i++) {
-          if(contourArea(cnts[i]) < 500) {
+          if(contourArea(cnts[i]) < 1500) {
               continue;
           }
 
           putText(frame, "Motion Detected", Point(10, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
       }
   
-      imshow("Camera", frame);
+    //   imshow("Camera", frame);
       
       if(waitKey(1) == 27){
           //exit if ESC is pressed
           break;
       }
+
+      std::vector<uchar> buff;
+
+    // encode to jpg
+    cv::imencode(".jpg", frame, buff);
+
+    // write jpg to stdout so it can be piped
+    fwrite(buff.data(), buff.size(), 1, stdout);
+    fflush(stdout);
+    k++;
   
   }
 
