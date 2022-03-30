118ac664e4d812e961ab4d6eee780b607f8b790b
Kyle Locquiao <klocquiao45@gmail.com>
2022-03-30 16:42:54 -0700

    Start camera module from main.cpp and change directory to make it easier to run the current makefile

--- monitor/main.cpp
+++ monitor/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+
 #include "receiver.h"
+#include "camera.h"
+
 int main(int argc, const char *argv[]) {
     startReceiver();
+    startCamera();
+
     while(1) {};
+    
     stopReceiver();
+    stopCamera();
     
     std::cout << "Our baby monitor is awesome\n";
     
