742b2fda1fd01b8895102d86f07db487bc033e50
Taha Liaqat <tliaqat@sfu.ca>
2022-04-06 00:08:53 -0700

    added audio mixer template

--- monitor/main.cpp
+++ monitor/main.cpp
@@ -1,11 +1,12 @@
 #include "camera.h"
 #include "receiver.h"
+#include "audioMixer_template.h"
 
 // Test program to convert a2d to wave.
 int main(int argc, const char *argv[]) {
     startReceiver();
     startCamera();
-
+    AudioMixer_init();
     while (1) {
     };
 
